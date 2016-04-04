01f83cf5c49270cbf5314d97e1cf82bd2b427a75
Denis <wiirlock@gmail.com>
2016-04-04 15:42:34 +0300

    Initial Commit

---
+++ Design_Patterns_ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Design_Patterns_ObjC
+//
+//  Created by Denis Andreev on 04.04.16.
+//  Copyright © 2016 Denis Andreev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
